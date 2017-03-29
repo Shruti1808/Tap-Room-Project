9a4a1a76b0d81a5f6e1a29fe5344f17e80c862fb
Marc Larkin and Shruti Priya <marc+shruti@>
2017-03-29 10:20:24 -0700

    fix errors with tsc command

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import  { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: `
+    <h1>Welcome to the Tap Room</h1>
+    `
+})
+
+export class AppComponent {
+
+}
