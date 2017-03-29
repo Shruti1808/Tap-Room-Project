7d102417b20122ab7a04751c4017d9cb9498273c
Marc Larkin and Shruti Priya <marc+shruti@>
2017-03-29 15:27:18 -0700

    add a new beer to the list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,52 @@
 import  { Component } from '@angular/core';
+import { Keg } from './app.kegs';
+
+ const Kegs: Keg[]  = [];
+
+ Kegs.push(new Keg('Bud Light', 'Anheuser-Busch', 111, 3.88, 124));
+ Kegs.push(new Keg('Rolling Rock', 'Anheuser-Busch', 167, 4.4, 124));
+ Kegs.push(new Keg('Blue Moon', 'Anheuser-Busch', 150, 5.4, 124));
+ Kegs.push(new Keg('LandShark Lager', 'Anheuser-Busch', 147, 4.7, 124));
+
+ for(let keg of Kegs){
+     console.log(keg.kegName);
+ }
 
 @Component({
     selector: 'app-root',
     template: `
     <h1>Welcome to the Tap Room</h1>
-    
+    <div *ngFor="let keg of kegs">
+    <hr>
+        <h3>{{keg.kegName}} by {{keg.kegBrand}}</h3>
+        <h4>Price:{{keg.price}}</h4>
+        <h4>Alcohol Content: {{keg.alcoholContent}}</h4>
+        
+    </div>
+
+    <form>
+        <input [(ngModel)] = "newKegName" type="text" placeholder="Name"name="Name">
+        <br>
+        <input [(ngModel)] = "newKegBrand" type="text" placeholder="Brand"name="Brand">
+        <br>
+        <input [(ngModel)] = "newPrice" type="text" placeholder="Price"name="Price">
+        <br>
+        <input [(ngModel)] = "newAlcoholContent" type="text" placeholder="Alcohol Content"name="Alcohol
+Content">
+        <br>
+        <input [(ngModel)] = "newQuantity" type="text" placeholder="Quantity"name="Quantity">
+        <br>
+        <button class="btn btn-primary" (click)="newBeer(newKegName, newKegBrand, newPrice, newAlcoholContent, newQuantity)">Add Beer</button>
+    </form>
     `
 })
 
 export class AppComponent {
+    kegs = Kegs;
+
+    newBeer(newKegName, newKegBrand, newPrice, newAlcoholContent, newQuantity) {
+        this.kegs.push(new Keg(newKegName, newKegBrand, newPrice, newAlcoholContent, newQuantity))
+    }
+
 
 }
---
+++ app/app.kegs.ts
@@ -0,0 +1,9 @@
+export class Keg {
+    empty: boolean = false;
+
+    constructor(public kegName: string, public kegBrand: string, public price: number, public alcoholContent: number,  public quantity: number){}
+
+    markEmpty(){
+        this.empty = true;
+    }
+}
