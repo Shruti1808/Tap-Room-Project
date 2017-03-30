19d722be75fcd5b3aad8183de009b4e1db63690c
Marc Larkin and Shruti Priya <marc+shruti@>
2017-03-30 16:43:03 -0700

    finish styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,12 @@ import {EditKegComponent} from './edit-keg.component';
 
  const Kegs: Keg[]  = [];
 
- Kegs.push(new Keg('Bud Light', 'Anheuser-Busch', 111, 3.88, 124));
+ Kegs.push(new Keg('Bud Light', 'Anheuser-Busch', 111, 3.88, 11));
  Kegs.push(new Keg('Rolling Rock', 'Anheuser-Busch', 167, 4.4, 124));
  Kegs.push(new Keg('Blue Moon', 'Anheuser-Busch', 150, 5.4, 124));
  Kegs.push(new Keg('LandShark Lager', 'Anheuser-Busch', 147, 4.7, 124));
+ Kegs.push(new Keg('Trashed', 'Anheuser-Busch', 147, 8.7, 124));
+ Kegs.push(new Keg('Coma', 'Anheuser-Busch', 147, 12.7, 124));
 
  for(let keg of Kegs){
      console.log(keg.kegName);
@@ -17,20 +19,23 @@ import {EditKegComponent} from './edit-keg.component';
 @Component({
     selector: 'app-root',
     template: `
-    <h1 class="test">Welcome to the Tap Room</h1>
-    <div class="beer-panel"  *ngFor="let keg of kegs">
+    <h1 class="welcome">Welcome to the Tap Room</h1>
+    <ul class="row">
+    <li *ngFor="let keg of kegs">
+    <div class="col-md-4 beer-panel"  >
     <div [class] = "quantityColor(keg)">
         <h3 class="beer-heading"><span class="beer-name">{{keg.kegName}}</span> <em>by</em> <span class="beer-brand">{{keg.kegBrand}}</span></h3>
         <h4 class>Price: {{keg.price}} /pint</h4>
-        <h4>Alcohol Content: {{keg.alcoholContent}}%</h4>
+        <h4>Alcohol Content: <span [class] = "beerStrength(keg)">{{keg.alcoholContent}}%</span></h4>
         <h4>Keg Quantity: {{keg.quantity}}</h4>
         <button (click) = "editKeg(keg)">Edit</button>
 
         <button (click)= "sellPint(keg)">Sell Pint</button>
     </div>
     </div>
+    </li>
+    </ul>
     <edit-kegs [selectedKeg] = "selectedKeg"></edit-kegs>
-
     <form>
         <input [(ngModel)] = "newKegName" type="text" placeholder="Name"name="Name">
         <br>
@@ -75,6 +80,17 @@ export class AppComponent {
         }
     }
 
+    beerStrength(keg) {
+        if(keg.alcoholContent > 11){
+            return "very-strong";
+        } else if(keg.alcoholContent > 7){
+            return "strong";
+        } else if(keg.alcoholContent > 5){
+            return "medium";
+        } else {
+            return "weak";
+        }
+    }
 
 
     // pintCost = function(kegCost) {
